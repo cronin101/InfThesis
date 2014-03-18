109f957ca950d285d4ac8a1ec7680c75350f5f73
Aaron Cronin <cronin@include.cat>
2014-03-18 13:34:07 +0000

    Design section

--- spinlatex.sh
+++ spinlatex.sh
@@ -1,1 +1,1 @@
-kicker -e '(pdflatex thesis.tex) && (bibtex thesis) && (pdflatex thesis.tex) && (texcount thesis.tex -inc -brief)' -c
+kicker -e '(pdflatex thesis.tex) && (bibtex thesis) && (pdflatex thesis.tex) && (texcount thesis.tex -inc -merge -brief)' -c
