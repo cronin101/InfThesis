efe871fb1b0bf1cf4078ba89700be99a0dab3bcc
Aaron Cronin <cronin@include.cat>
2014-04-01 15:32:50 +0100

    Prettyness++

--- spinlatex.sh
+++ spinlatex.sh
@@ -1,1 +1,1 @@
-kicker -e '(pdflatex thesis.tex) && (bibtex thesis) && (pdflatex thesis.tex) && (texcount thesis.tex -inc -merge -brief)' -c
+kicker -e '(xelatex thesis.tex) && (bibtex thesis) && (xelatex thesis.tex) && (texcount thesis.tex -inc -merge -brief)' -c
